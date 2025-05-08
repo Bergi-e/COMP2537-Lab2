0587447b46af8b3edb1e988d88eaa61dd014bb9c
Bergi-e <jayden.bergstrome@gmail.com>
2025-05-08 14:03:37 -0700

    Added empty ejs pages in a new views folder, and changed the app.get commands to render instead of send in preparation for migration

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3018;
 
-const fs = require('fs');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware to serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+const colors = ['blue', 'red', 'green'];
+const sizes = [20, 30, 40];
 // Index
 app.get('/', (req, res) => {
-    let html = fs.readFileSync(__dirname + '/app/html/index.html', 'utf8');
-    res.send(html);
+    res.render('index', {
+        css: ['index.css'],
+        js: ['index.js']
+    });
+    // let html = fs.readFileSync(__dirname + '/app/html/index.html', 'utf8');
+    // res.send(html);
 });
 
 // Color Indexes
+app.get('/:color', (req, res, next) => {
+    const { color } = req.params;
+    if (!colors.includes(color)) return next();
+    res.render('color', {
+        color,
+        css: [`${color}.css`],
+        js: []
+    });
+});
+
+/** 
 app.get('/blue', (req, res) => {
     let html = fs.readFileSync(__dirname + '/app/html/blue.html', 'utf8');
     res.send(html);
@@ -27,8 +46,21 @@ app.get('/green', (req, res) => {
     let html = fs.readFileSync(__dirname + '/app/html/green.html', 'utf8');
     res.send(html);
 });
+*/
 
 // Color Pages with Sizes
+app.get('./:color/:size', (req, res, next) => {
+    const { color, size } = req.params;
+    const sizeNum = parseInt(size, 10);
+    if (!colors.includes(color) || !sizes.includes(sizeNum)) return next();
+    res.render('color-size', {
+        color,
+        size: sizeNum,
+        css: [`${color}.css`, `font${size}.css`],
+        js: [`${color}.js`]
+    });
+});
+/** 
 app.get('/blue/20', (req, res) => {
     let html = fs.readFileSync(__dirname + '/app/html/blue20.html', 'utf8');
     res.send(html);
@@ -65,6 +97,7 @@ app.get('/green/40', (req, res) => {
     let html = fs.readFileSync(__dirname + '/app/html/green40.html', 'utf8');
     res.send(html);
 });
+*/
 
 // Start the server
 app.listen(port, () => {
